cbaca5cd7930cb740e4d19d52ccac0aafee5ff28
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-12-12 21:34:36 -0800

    Update main.cpp

--- client/client_arduinoUno/src/main.cpp
+++ client/client_arduinoUno/src/main.cpp
@@ -1,23 +1,24 @@
 #include <Arduino.h>
 
-const int floatSwitch = 2;
+const int floatSwitchPin = 2;
+const int relayPin = 4;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(floatSwitch, INPUT_PULLUP);
+  pinMode(floatSwitchPin, INPUT_PULLUP);
 }
 
 // Digital pin cannot supply enough current to power the peristaltic pump's dc motor
 // Need a relay, split into a power supply, connected to the peristaltic pump
 void loop() {
-  int switchFlag = digitalRead(floatSwitch);
+  int switchFlag = digitalRead(floatSwitchPin);
   Serial.println(switchFlag);
 
   if(switchFlag) {
-    digitalWrite(4, HIGH);
+    digitalWrite(relayPin, HIGH);
   } else {
-    digitalWrite(4, LOW);
+    digitalWrite(relayPin, LOW);
   }
   delay(500);
 }
