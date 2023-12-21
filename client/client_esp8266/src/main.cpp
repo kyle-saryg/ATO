1cc4c94bcc1cb8f27a2f04f40d3668221a18476f
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-12-20 19:11:57 -0800

    Code from Arduino prototype board

--- client/client_esp8266/src/main.cpp
+++ client/client_esp8266/src/main.cpp
@@ -1,10 +1,27 @@
 #include <Arduino.h>
 
+// Wire float switch to pin 2
+// Connect other end to GND
+const int floatSwitchPin = 2;
+const int relayPin = 4;
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(floatSwitchPin, INPUT_PULLUP);
+  pinMode(relayPin, OUTPUT);
 }
 
+// Digital pin cannot supply enough current to power the peristaltic pump's dc motor
+// Need a relay, split into a power supply, connected to the peristaltic pump
 void loop() {
-  Serial.println("Hello, World!");
-  delay(5000);
+  int switchFlag = digitalRead(floatSwitchPin);
+  Serial.println(switchFlag);
+
+  if(switchFlag) {
+    digitalWrite(relayPin, HIGH);
+  } else {
+    digitalWrite(relayPin, LOW);
+  }
+  delay(500);
 }
