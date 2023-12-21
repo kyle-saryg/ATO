e774f82cd23a00eae0f5c04d39d52673a80de6db
Kyle Sarygin <kyle.cuyos13@gmail.com>
2023-12-21 01:50:10 -0800

    1:49am I have work in the morning

--- client/client_esp8266-DeepSleep/src/main.cpp
+++ client/client_esp8266-DeepSleep/src/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 
 void doseQuarterLiter();
-void floatSwitchISR();
 
 /*
   WeMos D1:
@@ -15,36 +13,27 @@ void floatSwitchISR();
 const int floatSwitchPin = 2;
 // Relay input on pin 4
 const int relayPin = 4;
-// Accessed by the ISR, used by main loop()
-volatile boolean switchFlag = false;
 
 void setup() {
   Serial.begin(9600);
   pinMode(floatSwitchPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(floatSwitchPin), floatSwitchISR, FALLING);
   pinMode(relayPin, OUTPUT);
 }
 
 // Digital pin cannot supply enough current to power the peristaltic pump's dc motor
 // Need a relay, split into a power supply, connected to the peristaltic pump
 void loop() {
-  if (switchFlag) {
-    Serial.println("DOSING");
-    delay(2000);
-    switchFlag = false;
-  } else {
-    Serial.println("Not Dosing");
-    delay(2000);
-    switchFlag = false;
-  }
-}
+  // Read water level
+  int switchFlag = digitalRead(floatSwitchPin);
 
-void IRAM_ATTR floatSwitchISR() {
-  if (digitalRead(floatSwitchPin) == 0) {
-    switchFlag = true;
+  if(switchFlag == 0) {
+    Serial.println(switchFlag);
+    doseQuarterLiter();
   } else {
-    switchFlag = false;
+    digitalWrite(relayPin, LOW);
+    Serial.println(switchFlag);
   }
+  delay(500);
 }
 
 void doseQuarterLiter() {
